ff5730a32bc470161f82847babd475243d74d121
Mohamed Mohamud <m.mohamud1@hotmail.com>
2026-03-03 20:09:02 +0000

    Update Browser Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import SigninPage from './pages/signin';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path='/' component={Home} exact />
-        <Route path='/signin' component={SigninPage} exact />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SigninPage />} />
+      </Routes>
     </Router>
   );
 }
